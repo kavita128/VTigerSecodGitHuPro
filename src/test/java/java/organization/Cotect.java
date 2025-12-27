75ed4eb09fbce51d2f879576f541554681335862
kavita128 <janakiias123@gmail.com>
2025-12-27 12:23:23 +0530

    Add additional print statements in Cotect.java
    Added multiple print statements to the Cotect class.

--- src/test/java/java/organization/Cotect.java
+++ src/test/java/java/organization/Cotect.java
@@ -9,6 +9,10 @@ public void m1()
 	System.out.println("hey Lead Kavita");
 	System.out.println("you are so good");
 	System.out.println("keep goig");
+System.out.println("keep goig");
+	System.out.println("keep goig");
+	System.out.println("keep goig");
+	
 }
 
 	// TODO Auto-generated method stub
